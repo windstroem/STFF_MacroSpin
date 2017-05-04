fdfdbbd263f423eb730eeec5fd75fdf06ff53b5e
Thomas Windbacher <t.windbacher@gmail.com>
2017-05-04 13:08:52 +0200

    Added time stamp to output file. MTJ_A and MTJ_Q are now shown in two separate plots.

--- mA_mQ.py
+++ mA_mQ.py
@@ -8,6 +8,7 @@
 import os
 import io
 import time
+import datetime
 import math  as ma
 import numpy as np
 from numpy import linalg as la
@@ -39,7 +40,7 @@ alpha =  0.01; # 1
 K1    =  2.e5; # J/m^3
 K1vec =  np.array([0., 0., 1.]);# 1
 #Exchange constant
-Aexch =  2.e-11; # J/m
+Aexch =  4.e-11; # J/m
 #Magnetization saturation
 Ms    = 4.e5; # A/m 
 
@@ -66,7 +67,10 @@ c  = 3.e-09
 VA = a*a*c
 VB = VA
 VQ = a*1.*a*c;# in real device 2.*a
-V = VA+VQ;# + VB
+V  = VA+VQ;# + VB
+
+#Distance between MTJ_A and MTJ_Q
+d  = 3.e-08;# 30nmx30nmx3nm boxes
 
 #####################################################
 #Boundary conditions 
@@ -78,8 +82,8 @@ phi   = 0.2
 theta = 1.
 m0    = np.array([ma.cos(phi)*ma.sin(theta),ma.sin(phi)*ma.sin(theta),ma.cos(theta)])
 m0A   = np.array([ma.cos(phi)*ma.sin(theta),ma.sin(phi)*ma.sin(theta),ma.cos(theta)])
-m0Q   = np.array([ma.cos(0.1)*ma.sin(1.8),ma.sin(0.1)*ma.sin(1.8),ma.cos(1.8)])
-mAQ0  = np.hstack((m0,m0))
+m0Q   = np.array([ma.cos(0.1)*ma.sin(1.5),ma.sin(0.1)*ma.sin(1.5),ma.cos(1.5)])
+mAQ0  = np.hstack((m0A,m0Q))
 #print(mAQ0)
 #Start and end of simulation time
 t0    = 0.
@@ -91,6 +95,23 @@ t     = np.array([t0])
 
 #Prefactor for RHS of equation
 prefactor = -1.*gamma/(1+alpha*alpha)
+
+######################################################################
+#Alternativ Runge-Kutta solver
+######################################################################
+def RK4(f):
+    return lambda t, y, dt: (
+            lambda dy1: (
+            lambda dy2: (
+            lambda dy3: (
+            lambda dy4: (dy1 + 2*dy2 + 2*dy3 + dy4)/6
+            )( dt * f( t + dt  , y + dy3   ) )
+	    )( dt * f( t + dt/2, y + dy2/2 ) )
+	    )( dt * f( t + dt/2, y + dy1/2 ) )
+	    )( dt * f( t       , y         ) )
+ 
+
+
 
 ######################################################################
 #Define functions for effective field calculations
@@ -141,9 +162,9 @@ def Htherm(T, alpha, gamma, V, Ms, dt) :
  return ma.sqrt((2.*kb*T*alpha)/(gamma*V*mu0*Ms*dt*(1.+alpha*alpha)))*np.array(np.random.normal(0.,1.,3));
 
 #Defining the effective field term
-def Heff(m, Ms, N, K1vec, K1,alpha,gamma,V,T,dt):
+def Heff(m, Ms, N, K1vec, K1,alpha,gamma,V,T,dt,mn,d,A,r):
  "Effective field containing all physical contributions"
- return Huni(m, K1vec, Ms, K1) + Hdem(m, N, Ms) + Htherm(T,alpha,gamma,V,Ms,dt) 
+ return Huni(m, K1vec, Ms, K1) + Hdem(m, N, Ms) + Htherm(T,alpha,gamma,V,Ms,dt) + Hexch(m,mn,d,A,Ms) + Hstray(m,r,Ms,V) 
 
 
 #Definition of the spin transfer torque terms
@@ -163,20 +184,22 @@ def tau (m, s, I, p, V, Ms, gamma, epsilon, g) :
  return (hbar*I)/(Ms*V*qe)*g*( mxmxs - epsilon*mxs );
 
 #Define right hand side of the ODE
-def f(t,m,prefactor,Ms,N,K1vec,K1,alpha,gamma,V,Temp,dt,p,s,epsilon,I):
+def f(t,m,prefactor,Ms,NA,NQ,K1vec,K1,alpha,gamma,VA,VQ,Temp,dt,p,s,epsilon,I,d,A):
  "RHS of LLG-ODE"
  #Split m into two 3x1 normalized vectors
  mA= m[0:3]/la.norm(m[0:3])
  mQ= m[3:6]/la.norm(m[3:6])
- heffA      = Heff(mA,Ms,N,K1vec,K1,alpha,gamma,V,Temp,dt)
+ #Calculations for MTJ_A
+ heffA      = Heff(mA,Ms,NA,K1vec,K1,alpha,gamma,VA,Temp,dt,mQ,d,A,d*np.array([1.,0.,0.]))
  precesionA = np.cross(mA,heffA)
  dampingA   = alpha*np.cross(mA,precesionA)
- sttA       = tau(mA,s,I,p,V,Ms,gamma,epsilon,gox(p,mA,s))
+ sttA       = tau(mA,s,I,p,VA,Ms,gamma,epsilon,gox(p,mA,s))
  rhsA       = precesionA + dampingA + sttA
- heffQ      = Heff(mQ,Ms,N,K1vec,K1,alpha,gamma,V,Temp,dt)
+ #Calculations for MTJ_Q
+ heffQ      = Heff(mQ,Ms,NQ,K1vec,K1,alpha,gamma,VQ,Temp,dt,mA,d,A,d*np.array([-1.,0.,0.]))
  precesionQ = np.cross(mQ,heffQ)
  dampingQ   = alpha*np.cross(mQ,precesionQ)
- sttQ       = tau(mQ,s,I,p,V,Ms,gamma,epsilon,gox(p,mQ,s))
+ sttQ       = tau(mQ,s,I,p,VQ,Ms,gamma,epsilon,gox(p,mQ,s))
  rhsQ       = precesionQ + dampingQ + sttQ
  #merge mA and mQ again for sending back
  rhs         = np.hstack((rhsA,rhsQ))
@@ -184,7 +207,9 @@ def f(t,m,prefactor,Ms,N,K1vec,K1,alpha,gamma,V,Temp,dt,p,s,epsilon,I):
 
 # Geometry dependent but constant.
 # It is sufficient to calculate only once before the integration
-N = np.array([Dx(a,b,c),Dy(a,b,c),Dz(a,b,c)])
+N  = np.array([Dx(a,b,c),Dy(a,b,c),Dz(a,b,c)])
+NA = np.array([Dx(a,a,c),Dy(a,a,c),Dz(a,a,c)])
+NQ = np.array([Dx(a,b,c),Dy(a,b,c),Dz(a,b,c)])
 
 ###################################################################
 #Functions for data manipulation and export
@@ -229,19 +254,23 @@ def write_data(filename,header,t,y):
 #dfactor : float Maximum factor to increase/decrease step size by in one step
 #beta : float Beta parameter for stabilised step size control.
 # verbosity : int Switch for printing messages (< 0 for no messages).
-arguments = (prefactor,Ms,N,K1vec,K1,alpha,gamma,V,Temp,dt,p,s,epsilon,I)
-r = ode(f).set_integrator('dopri5',first_step=dt,max_step=dt,nsteps=1e6,atol=1.e-6)
+arguments = (prefactor,Ms,N,N,K1vec,K1,alpha,gamma,VA,VQ,Temp,dt,p,s,epsilon,I,d,Aexch)
+r = ode(f).set_integrator('dopri5',first_step=dt,max_step=dt,nsteps=1e6,atol=1.e-3)
 r.set_initial_value(mAQ0, t0).set_f_params(*arguments)
 #
 while r.successful() and r.t < tend:
       r.integrate(r.t+dt)
       t = np.append(t,r.t)
-      print("t: %3.6e, %3.3f%%, %8i seconds" % (r.t, r.t/tend*100., time.clock() - start_time), end="\r" )
+      print("t: %3.6e, %3.3f%%, %s " % (t[-1], t[-1]/tend*100.,datetime.timedelta(seconds=(time.clock() - start_time))), end="\r" )
+      #print("t: %3.6e, %3.3f%%, %8i seconds" % (r.t, r.t/tend*100., time.clock() - start_time), end="\r" )
       sol  = np.append(sol,np.array([r.y]),axis=0)
-print("t: %3.6e, %3.3f%%, %8i seconds" % (t[-1], t[-1]/tend*100., time.clock() - start_time), end="\n" )
+#print("t: %3.6e, %3.3f%%, %8i seconds" % (t[-1], t[-1]/tend*100., time.clock() - start_time), end="\n" )
+print("t: %3.6e, %3.3f%%, %s " % (t[-1], t[-1]/tend*100.,datetime.timedelta(seconds=(time.clock() - start_time))), end="\n" )
 ##########################################################################################
 ##Visualization of simulation results
 ##########################################################################################
+plt.figure(1)
+plt.subplot(211)
 ##MTJ A
 #mx(t)
 plt.plot(t, sol[:,0],label='mx_A')
@@ -249,7 +278,15 @@ plt.plot(t, sol[:,0],label='mx_A')
 plt.plot(t, sol[:,1],label='my_A')
 #mz(t)
 plt.plot(t, sol[:,2],label='mz_A')
+#turn on legend
+plt.legend()
+#set x- and y-axis labels
+plt.ylabel('normalized magnetization (1)')
+plt.xlabel('time (s)')
+plt.grid(True)
+
 ##MTJ Q
+plt.subplot(212)
 #mx(t)
 plt.plot(t, sol[:,3],label='mx_Q')
 #my(t)
@@ -265,10 +302,18 @@ plt.grid(True)
 #put it on the display
 plt.show()
 
-#write_data('test_'+str(time.clock())+'.crv',create_header(),t,sol)
+write_data('test_'+str(time.strftime("%Y%d%m%H%M%S", time.localtime()))+'.crv',create_header(),t,sol)
+#print('test_'+str(time.strftime("%Y%d%m%H%M%S",time.localtime()))+'.crv')
 
 ############################################################################################Scratchpad and debug section
 #########################################################################################
-
+#dy = RK4(lambda t, y: f(...))
+# 
+#t, y, dt = 0., 1., .1
+#while t <= 10:
+#    if abs(round(t) - t) < 1e-5:
+#	print("y(%2.1f)\t= %4.6f \t" % ( t, y))
+#    t, y = t + dt, y + dy( t, y, dt )
+ 
 
 
